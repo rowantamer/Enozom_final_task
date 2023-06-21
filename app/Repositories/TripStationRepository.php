a2f02314a4e5b93336588b425116f9e2d7b71042
rowan <rowantamer5@gmail.com>
2023-06-21 16:32:34 +0300

    edit subquery

--- app/Repositories/TripStationRepository.php
+++ app/Repositories/TripStationRepository.php
@@ -11,18 +11,17 @@ class TripStationRepository implements ITripStationRepository
     {
         $trips = TripStation::join('trips', 'trip_stations.trip_id', '=', 'trips.id')
             ->join('trains', 'trips.train_id', '=', 'trains.id')
-            ->join('trip_stations AS ts2', function ($join) use ($fromId, $toId) {
-                $join->on('trips.id', '=', 'ts2.trip_id')
-                    ->where('trip_stations.station_id', '=', $fromId)
-                    ->where('ts2.station_id', '=', $toId)
-                    ->whereRaw('trip_stations.stop_time <= ts2.stop_time');
-            })
+            ->join('trip_stations AS ts2', 'trips.id', '=', 'ts2.trip_id')
+            ->where('trip_stations.station_id', '=', $fromId)
+            ->where('ts2.station_id', '=', $toId)
+            ->whereRaw('trip_stations.stop_time <= ts2.stop_time')
             ->select('trips.trips_id', 'trains.train_name', 'trip_stations.stop_time')
             ->distinct()
             ->get();
 
         return $trips;
     }
+
 
 
 
